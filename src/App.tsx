534676cf7f211b703119daa9485e76114a45d01e
Veranika Smiayun <veronika@MacBook-Pro-Veronika.local>
2023-10-28 22:18:42 +0300

    feat: add InputField and implemented receiving data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Component } from 'react';
+
+import InputField from './components/InputField';
+
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    items: [],
+  };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  getDate = (query?: string) => {
+    fetch(
+      `https://rickandmortyapi.com/api/character/
+      ${query ? `?name=${query}` : ''}`
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.setState({
+            isLoaded: true,
+            items: result.results,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  };
+
+  componentDidMount() {
+    this.getDate();
+  }
+
+  render() {
+    return (
+      <>
+        <InputField getDate={this.getDate} />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,47 @@
+import { Component, FormEvent } from 'react';
+
+interface InputFieldProps {
+  value: string;
+}
+
+class InputField extends Component<
+  { getDate: (request: string) => void },
+  InputFieldProps
+> {
+  constructor(props: { getDate: (request: string) => void }) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    localStorage.setItem('searchRequest', this.state.value);
+    this.props.getDate(this.state.value);
+  }
+
+  handleChange(searchRequest: string) {
+    this.setState({ value: searchRequest.trim() });
+  }
+
+  render() {
+    return (
+      <form className="input" onSubmit={(e) => this.handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Enter request"
+          className="input__box"
+          value={this.state.value}
+          onChange={(e) => this.handleChange(e.target.value)}
+        />
+        <button type="submit" className="input__submit">
+          Go
+        </button>
+      </form>
+    );
+  }
+}
+
+export default InputField;
