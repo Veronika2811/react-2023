5edefe1db39bd1f4b64937d47ab9cd015b205b31
Veronika <smeyun@list.ru>
2023-11-02 18:41:30 +0300

    fix: fix styles NothingFound component

--- src/components/NothingFound.tsx
+++ src/components/NothingFound.tsx
@@ -1,6 +1,8 @@
 const NothingFound = () => {
   return (
-    <h2>Sorry, but we couldn&apos;t find anything matching your request.</h2>
+    <div className="error__wrapper">
+      <h2>Sorry, but we couldn&apos;t find anything matching your request.</h2>
+    </div>
   );
 };
 
